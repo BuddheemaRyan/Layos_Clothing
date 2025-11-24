44f4e9e973cba41f31a0972a6b51afab5a43e0f6
Buddheema Ryan <buddimaraya16@gmail.com>
2025-11-24 18:00:21 +0530

    Initial Commit

---
+++ src/main/java/edu/icet/ecom/model/dto/Products.java
@@ -0,0 +1,17 @@
+package edu.icet.ecom.model.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Products {
+    private int id;
+    private String  name;
+    private String category;
+    private double price;
+    private int stock;
+    private String image;
+}
---
+++ src/main/java/edu/icet/ecom/repository/ProductRepository.java
@@ -0,0 +1,6 @@
+package edu.icet.ecom.repository;
+
+public class ProductRepository {
+
+
+}
